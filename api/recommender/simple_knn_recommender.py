f02954a91c1ccfc8fb11b50da077925949e8336a
Haeun-Oh <haeunoh.tech@gmail.com>
2020-12-30 19:57:32 +0900

    fix: update test matrix

--- api/recommender/simple_knn_recommender.py
+++ api/recommender/simple_knn_recommender.py
@@ -4,6 +4,7 @@ from collections import Counter
 import numpy as np
 import scipy.sparse as spr
 import pandas as pd
+import copy
 
 class SimpleKNNRecommender(RecInterface):
     song_meta = pd.read_json("song_meta.json")
@@ -13,16 +14,18 @@ class SimpleKNNRecommender(RecInterface):
     n_songs = len(song_meta)
 
     train_songs_A=[]
+    test_songs_A = []
 
     def rec(self, test_id):
-        p = np.zeros((self.n_songs, 1))
-
+        p1 = copy.deepcopy(self.test_songs_A)
+        pt = p1.T
+        p = pt.toarray()
         songs_already = test_id
 
         simpls = self.train_songs_A.dot(p)
         simpls2 = np.zeros_like(simpls)
 
-        inds = simpls.reshape(-1).argsort()[-9000:][::-1]  # .reshape(-1) == .reshape(1, -1)처럼 1차원 배열 반환
+        inds = simpls.reshape(-1).argsort()[-100:][::-1]  # .reshape(-1) == .reshape(1, -1)처럼 1차원 배열 반환
         vals = simpls[inds]
 
         m = np.max(vals)
@@ -35,11 +38,10 @@ class SimpleKNNRecommender(RecInterface):
         train_songs_A_T = self.train_songs_A.T.tocsr()
 
         cand_song = train_songs_A_T[:, inds].dot(vals2)
-        cand_song_idx = cand_song.reshape(-1).argsort()[-100:][::-1]  # 내림차순 정렬
+        cand_song_idx = cand_song.reshape(-1).argsort()[-50:][::-1]
 
-        cand_song_idx = cand_song_idx[np.isin(cand_song_idx, songs_already) == False][ :30]  # playlist에 원래 있던 song이 아닌 것들 100개
+        cand_song_idx = cand_song_idx[np.isin(cand_song_idx, songs_already) == False][ :15]  # playlist에 원래 있던 song이 아닌 것들 15개
         rec_song_idx = [i for i in cand_song_idx]
-        print(rec_song_idx)
 
         return rec_song_idx
 
@@ -48,7 +50,6 @@ class SimpleKNNRecommender(RecInterface):
         :return: (List) song_ids of the recommended playlist (추천할 song id list를 return하도록 구현)
         """
         test_plylist = [{'id':120000, 'songs':user_playlist}] #train의 playlist id가 115071번까지 있어서 120000
-        print(test_plylist)
         test = pd.DataFrame(data=test_plylist)
         plylst = pd.concat([self.train_plylist, test], ignore_index=True) # train + test
 
@@ -67,6 +68,12 @@ class SimpleKNNRecommender(RecInterface):
         dat = np.repeat(1, plylst_train['num_songs'].sum())
         self.train_songs_A = spr.csr_matrix((dat, (row, col)), shape=(self.n_train, self.n_songs))
         # -> train playlist와 song의 co-occurence matrix
+
+        test_row = np.repeat(range(1), plylst_test['num_songs'])
+        test_col = [song for songs in plylst_test['songs'] for song in songs]
+        test_dat = np.repeat(1, plylst_test['num_songs'].sum())
+        self.test_songs_A = spr.csr_matrix((test_dat, (test_row, test_col)), shape=(1, self.n_songs))
+        # -> test playlist와 song의 co-occurence matrix
 
         answers = self.rec(plylst_test.index)
         return answers
